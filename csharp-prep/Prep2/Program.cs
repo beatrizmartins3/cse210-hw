3a62e5328285370ce5bf80022b69e812933bd4b7
Beatriz Martins <biiam3462@gmail.com>
2023-04-19 11:48:50 -0300

    Prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string input=Console.ReadLine();
+        int number=int.Parse(input);
+
+        string letter="" ;
+
+        if (number>=90)
+        {
+            letter= "A";
+        }  
+        else if(number>=80)
+        {
+            letter="B";
+        }
+        else if (number>=70)
+        {
+            letter="C";
+        }
+        else if(number>=60)
+        {
+            letter="D";
+        }
+        else
+        {
+            letter="F";
+        }
+
+        if (number>=60)
+        {
+            Console.WriteLine($"Your letter grade is {letter}. Congratulations, you passed.");
+        }
+        else
+        {
+            Console.WriteLine($"Your letter grade is {letter}. You can do it next time.");
+        }
     }
+    
 }
