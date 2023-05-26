0d6f615130423a76844bc40a9cea202a3c0c165c
Beatriz Martins <biiam3462@gmail.com>
2023-05-26 12:05:15 -0300

    review

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,34 @@
 using System;
 
-class Program
+namespace MyApplication
 {
+  class Scripture
+  {
+    private string _scripture;
+    private string _reference;
+    private int chapter;
+    private int verse;
+
+    // Create a class constructor with multiple parameters
+    public Scripture(string scripReference, int scripChapter, int scripVerse, string scripText)
+    {
+      _scripture = scripText;
+      _reference = scripReference;
+      chapter = scripChapter;
+      verse=scripVerse;
+
+      _scripture="If ye love me, keep my commandments";
+      _reference="John";
+      chapter=14;
+      verse=15;
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+      Scripture text = new Scripture(" ",0,0," ");
+      Console.WriteLine(text._reference + " " + text.chapter + " : " + text.verse + " = " + text._scripture);
+
+      
     }
+  }
 }
